28dff798fae2a918b51feccb42a43201d39477aa
Daniel Kim <danielkim802@yahoo.com>
2017-01-16 06:20:09 -0800

    testing circuits; got 4 bit adder working

--- src/Main.java
+++ src/Main.java
@@ -14,16 +14,11 @@ public class Main {
         fulladder.addOutput("S");
         fulladder.addOutput("Co");
 
-        Constant A = new Constant(0);
-        Constant B = new Constant(0);
-        Constant C = new Constant(0);
         Xor xor1 = new Xor();
         Xor xor2 = new Xor();
         And and1 = new And();
         And and2 = new And();
         Or or1 = new Or();
-        Output S = new Output();
-        Output Co = new Output();
 
         fulladder.getInput("A").connect("0", xor1);
         fulladder.getInput("A").connect("1", and2);
@@ -40,34 +35,58 @@ public class Main {
 
         fulladder.setComponents(Arrays.asList(xor1, xor2, and1, and2, or1));
 
-        A.connect("A", fulladder);
-        B.connect("B", fulladder);
-        C.connect("C", fulladder);
-        fulladder.connect("S", "input", S);
-        fulladder.connect("Co", "input", Co);
+        long[] A = {1,0,1,1};
+        long[] B = {0,0,0,1};
 
-        A.propagate();
-        B.propagate();
+        Constant A0 = new Constant(A[3]);
+        Constant A1 = new Constant(A[2]);
+        Constant A2 = new Constant(A[1]);
+        Constant A3 = new Constant(A[0]);
+        Constant B0 = new Constant(B[3]);
+        Constant B1 = new Constant(B[2]);
+        Constant B2 = new Constant(B[1]);
+        Constant B3 = new Constant(B[0]);
+        Constant C = new Constant(1);
+        Output S0 = new Output();
+        Output S1 = new Output();
+        Output S2 = new Output();
+        Output S3 = new Output();
+        Output Co = new Output();
+
+        Circuit f0 = fulladder.copy();
+        Circuit f1 = fulladder.copy();
+        Circuit f2 = fulladder.copy();
+        Circuit f3 = fulladder.copy();
+
+        A0.connect("A", f0);
+        A1.connect("A", f1);
+        A2.connect("A", f2);
+        A3.connect("A", f3);
+        B0.connect("B", f0);
+        B1.connect("B", f1);
+        B2.connect("B", f2);
+        B3.connect("B", f3);
+        C.connect("C", f0);
+        f0.connect("Co", "C", f1);
+        f1.connect("Co", "C", f2);
+        f2.connect("Co", "C", f3);
+        f3.connect("Co", "input", Co);
+        f0.connect("S", "input", S0);
+        f1.connect("S", "input", S1);
+        f2.connect("S", "input", S2);
+        f3.connect("S", "input", S3);
+
+        A0.propagate();
+        A1.propagate();
+        A2.propagate();
+        A3.propagate();
+        B0.propagate();
+        B1.propagate();
+        B2.propagate();
+        B3.propagate();
         C.propagate();
 
-        Constant A2 = new Constant(0);
-        Constant B2 = new Constant(0);
-        Constant C2 = new Constant(1);
-        Output S2 = new Output();
-        Output Co2 = new Output();
-
-        Circuit fulladder2 = fulladder.copy();
-        A2.connect("A", fulladder2);
-        B2.connect("B", fulladder2);
-        C2.connect("C", fulladder2);
-        fulladder2.connect("S", "input", S2);
-        fulladder2.connect("Co", "input", Co2);
-
-        A2.propagate();
-        B2.propagate();
-        C2.propagate();
-
-        System.out.println(S2.value());
-        System.out.println(Co2.value());
+        System.out.println(""+S3.value()+S2.value()+S1.value()+S0.value());
+        System.out.println(Co.value());
     }
 }
